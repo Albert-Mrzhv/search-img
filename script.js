11186a1f6b8f60e0cd9a008feb616c45a9ab28bc
Albert_Mr <gds_dhj@mail.ru>
2025-02-05 20:53:37 +0300

    img-point

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ searchBtn.addEventListener('click', () => {
         setTimeout(() => {
             result.innerHTML = `
             <div class="img-search">
-                <img class="img" src="/img-cars/1701416671_sportishka-com-p-samie-luchshie-sportkari-instagram-16.jpg" alt="">
-                <img class="img" src="/img-cars/861917.jpg" alt="">
-                <img class="img" src="/img-cars/avto-hammer.orig.jpg" alt="">
-                <img class="img" src="/img-cars/cattouchret.webp" alt="">
-                <img class="img" src="/img-cars/FqaNNR-TY3M.jpg" alt="">
-                <img class="img" src="/img-cars/kAAAAgN4A-A-960.jpg" alt="">
+                <img class="img" src="./img-cars/1701416671_sportishka-com-p-samie-luchshie-sportkari-instagram-16.jpg" alt="">
+                <img class="img" src="./img-cars/861917.jpg" alt="">
+                <img class="img" src="./img-cars/avto-hammer.orig.jpg" alt="">
+                <img class="img" src="./img-cars/cattouchret.webp" alt="">
+                <img class="img" src="./img-cars/FqaNNR-TY3M.jpg" alt="">
+                <img class="img" src="./img-cars/kAAAAgN4A-A-960.jpg" alt="">
             </div>
             `
         }, 2000);
@@ -30,12 +30,12 @@ searchBtn.addEventListener('click', () => {
         setTimeout(() => {
             result.innerHTML = `
             <div class="img-search">
-                <img class="img" src="/img-home/1633327009_1-p-foto-finskikh-domov-i-kottedzhei-foto-1.jpg" alt="">
-                <img class="img" src="/img-home/1686202736_gagaru-club-p-nebolshoi-kirpichnii-domik-dizain-vkontakt-37.jpg" alt="">
-                <img class="img" src="/img-home/354580-krasivyy-dom-33.jpg" alt="">
-                <img class="img" src="/img-home/main-spb-18-1.jpg" alt="">
-                <img class="img" src="/img-home/orig.jpg" alt="">
-                <img class="img" src="/img-home/XXL_height.webp" alt="">
+                <img class="img" src="./img-home/1633327009_1-p-foto-finskikh-domov-i-kottedzhei-foto-1.jpg" alt="">
+                <img class="img" src="./img-home/1686202736_gagaru-club-p-nebolshoi-kirpichnii-domik-dizain-vkontakt-37.jpg" alt="">
+                <img class="img" src="./img-home/354580-krasivyy-dom-33.jpg" alt="">
+                <img class="img" src="./img-home/main-spb-18-1.jpg" alt="">
+                <img class="img" src="./img-home/orig.jpg" alt="">
+                <img class="img" src="./img-home/XXL_height.webp" alt="">
             </div>
             `
         }, 2000);
